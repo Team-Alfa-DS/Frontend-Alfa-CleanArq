504f4e789cf9d56edcae61c057815b766d7d70e2
dimeBrii <thiswasbrii@gmail.com>
2024-06-09 11:46:34 -0400

    ✨ Feat : adding users use cases

---
+++ lib/aplication/use_cases/user/forgot_password_use_case.dart
@@ -0,0 +1,30 @@
+import 'package:alpha_gymnastic_center/aplication/localStorage/local_storage.dart';
+import 'package:alpha_gymnastic_center/common/result.dart';
+import 'package:alpha_gymnastic_center/common/use_case.dart';
+import 'package:alpha_gymnastic_center/domain/interfaces/user_interfaces.dart';
+import 'package:alpha_gymnastic_center/domain/repositories/user_repository.dart';
+
+class ForgotPasswordUseCaseInput extends IUseCaseInput {
+  final String email;
+
+  ForgotPasswordUseCaseInput({required this.email});
+}
+
+class ForgotPasswordUseCase extends IUseCase<ForgotPasswordUseCaseInput, void> {
+  final UserRepository userRepository;
+  final LocalStorage localStorage;
+
+  ForgotPasswordUseCase(
+      {required this.userRepository, required this.localStorage});
+
+  @override
+  Future<Result<void>> execute(ForgotPasswordUseCaseInput params) async {
+    ForgetPasswordRequest forgetPasswordRequest =
+        ForgetPasswordRequest(email: params.email);
+    final result = await userRepository.forgetPassword(forgetPasswordRequest);
+    if (result.hasValue()) {
+      //! pin pong
+    }
+    return result;
+  }
+}
