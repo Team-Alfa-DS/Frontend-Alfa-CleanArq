dfb1454c356eeaaf7cddb856afa36b7746d2ad0b
GelArria <50898259+GelArria@users.noreply.github.com>
2024-06-17 00:09:25 -0400

    Blogs (BLoC, Repos, Eventos y manejo de Estados puestos como base) para Carga de carrousel.

---
+++ lib/aplication/use_cases/blog/get_blog_many_use_case.dart
@@ -0,0 +1,39 @@
+import '../../../common/result.dart';
+import '../../../common/use_case.dart';
+import '../../../domain/entities/blog.dart';
+import '../../../domain/repositories/blog_repository.dart';
+
+class GetBlogDataUseCaseInput extends IUseCaseInput {
+  final String filter;
+  final int page;
+  final int perPage;
+  final String trainer;
+  final String category;
+
+  GetBlogDataUseCaseInput({
+    required this.filter,
+    required this.page,
+    required this.perPage,
+    required this.trainer,
+    required this.category
+  });
+
+}
+
+class GetBlogDataUseCase
+    implements IUseCase<GetBlogDataUseCaseInput, List<Blog>> {
+  final BlogRepository blogRepository;
+
+  GetBlogDataUseCase({required this.blogRepository});
+
+  @override
+  Future<Result<List<Blog>>> execute(GetBlogDataUseCaseInput params) async {
+    print('Entro en getMany');
+    return await blogRepository.checkManyBlogs(
+        page: params.page,
+        perpage: params.perPage,
+        filter: params.filter,
+        trainer: params.trainer,
+        category: params.category);
+  }
+}
---
+++ lib/infraestructure/presentation/widgets/blogsCaursel.dart
@@ -0,0 +1,56 @@
+import 'package:alpha_gymnastic_center/infraestructure/presentation/widgets/scrollHorizontal.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../../aplication/BLoC/blog/blog_many/blog_many_bloc.dart';
+import '../../../aplication/BLoC/blog/blog_many/blog_many_event.dart';
+import '../../../aplication/BLoC/blog/blog_many/blog_many_state.dart';
+import '../../../aplication/use_cases/blog/get_blog_many_use_case.dart';
+
+class blogsCarousel extends StatelessWidget {
+  const blogsCarousel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) =>
+      BlogListBloc(GetIt.instance<GetBlogDataUseCase>())
+
+      //ok ahora aca cuando construya esto hay que poner el trainer para probar y la categoria cuando se toca en el homescreen se refresca esta llamada
+        ..add(const LoadBlogList(page: 1, perPage: 7, filter: "POPULAR", trainer: '', category: '')),
+      child: BlocBuilder<BlogListBloc, BlogListState>(
+        builder: (context, state) {
+          if (state is BlogListLoading) {
+            return const CircularProgressIndicator();
+          } else if (state is BlogListLoaded) {
+            final blogs = state.blogs.sublist(0, 3);
+            return SizedBox(
+              height: 195,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: blogs.map((blogs) {
+                  return ScrollHorizontal(
+                    titulo: blogs.trainer!.name,
+                    descripcion: blogs.title,
+                    categoria: blogs.category!,
+                    fecha: blogs.date.toString(),
+                    foto: blogs.images!.first.toString(),
+                    disposicion: 2,
+                    isNew: false,
+                    conexion: "/blogs",
+                  );
+                }).toList(),
+              ),
+            );
+          } else if (state is BlogListFailed) {
+            return Text('Error: ${state.failure}');
+          } else {
+            return const SizedBox.shrink();
+          }
+        },
+      ),
+    );
+  }
+}
