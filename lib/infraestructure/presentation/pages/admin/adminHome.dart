0acae62035df5eaa04b8e92cbad5d808d8d2bd14
Leo1725 <rlsantos1725@gmail.com>
2024-07-06 02:58:30 -0400

    Admin User Screen

---
+++ lib/infraestructure/presentation/pages/admin/adminHome.dart
@@ -0,0 +1,149 @@
+import 'package:alpha_gymnastic_center/infraestructure/presentation/pages/admin/commons/COlors.dart';
+import 'package:alpha_gymnastic_center/infraestructure/presentation/pages/course/Course.dart';
+import 'package:flutter/material.dart';
+
+class AdminDashboardScreen extends StatelessWidget {
+  const AdminDashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const YogaAppBar(title: "Dashboard Administrador"),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: AppColors.secondaryColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Row(
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage:
+                        NetworkImage('https://via.placeholder.com/150'),
+                  ),
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Leo Hernandez',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'HernandezL@gmail.com',
+                        style: TextStyle(fontSize: 16, color: Colors.grey),
+                      ),
+                      Text(
+                        '+58 424 131 2528',
+                        style: TextStyle(fontSize: 16, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            // Mensaje de bienvenida
+            const Text(
+              'Bienvenido(a) al panel de administración',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 60),
+
+            // Botones de management
+            GridView.count(
+              crossAxisCount: 1,
+              childAspectRatio: 5,
+              shrinkWrap: true,
+              children: [
+                _ManagementButton(
+                  title: 'Users',
+                  onPressed: () {
+                    //Navigator.pushNamed(context, '/user-management');
+                    // Navegar a la pantalla de usuarios
+                  },
+                  backgroundColor: AppColors.primaryLightColor,
+                ),
+                _ManagementButton(
+                  title: 'Courses',
+                  onPressed: () {
+                    // Navegar a la pantalla de cursos
+                  },
+                  backgroundColor: AppColors.primaryLightColor,
+                ),
+                _ManagementButton(
+                  title: 'Blogs',
+                  onPressed: () {
+                    // Navegar a la pantalla de blogs
+                  },
+                  backgroundColor: AppColors.primaryLightColor,
+                ),
+                _ManagementButton(
+                  title: 'Categorys',
+                  onPressed: () {
+                    //Navigator.pushNamed(context, '/category-management');
+                    // Navegar a la pantalla de categorías
+                  },
+                  backgroundColor: AppColors.primaryLightColor,
+                ),
+                _ManagementButton(
+                  title: 'Trainers',
+                  onPressed: () {
+                    // Navegar a la pantalla de entrenadores
+                    //Navigator.pushNamed(context, '/trainer-management');
+                  },
+                  backgroundColor: AppColors.primaryLightColor,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ManagementButton extends StatelessWidget {
+  final String title;
+  final VoidCallback onPressed;
+  final Color backgroundColor;
+
+  const _ManagementButton(
+      {required this.title,
+      required this.onPressed,
+      required this.backgroundColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      color: backgroundColor,
+      child: InkWell(
+        onTap: onPressed,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/infraestructure/presentation/pages/admin/commons/COlors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors {
+  static const primaryColor = Color.fromRGBO(103, 58, 183, 1);
+  static const primaryLightColor = Color.fromRGBO(123, 78, 203, 1);
+  static const primaryDarkColor = Color.fromRGBO(83, 38, 163, 1);
+  static const secondaryColor = Color.fromRGBO(255, 255, 255, 1);
+  static const backgroundColor = Color.fromRGBO(242, 242, 242, 1);
+  //static const Color primaryLightColor = Color(0xFF4CAF50);
+  static const Color errorColor = Color(0xFFE57373);
+}
