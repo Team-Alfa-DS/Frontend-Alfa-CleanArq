57c5ddc7bcc1dc179253371c3555a730810660ad
Gianpiero Yorlano <gianpieroy@gmail.com>
2024-07-09 22:46:43 -0400

    Arreglo para endpoints

--- lib/domain/repositories/notification_repository.dart
+++ lib/domain/repositories/notification_repository.dart
@@ -2,10 +2,7 @@ import 'package:alpha_gymnastic_center/common/result.dart';
 import 'package:alpha_gymnastic_center/domain/entities/notification.dart';
 
 abstract class NotificationRepository {
-  Future<Result<List<Notifications>>> getNotificationMany({
-    required int page,
-    required int perPage,
-  });
+  Future<Result<List<Notifications>>> getNotificationMany();
   Future<Result<int>> getNotificationNotReaded();
   Future<Result<Notifications>> getSingleNotification({required String id});
   Future<Result<bool>> deleteNotifications();
